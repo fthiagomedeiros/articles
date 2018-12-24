424ceee5c3653b32eb5ddb10bbb5fdae96ab8c53
Thiago Medeiros <fthiagomedeiros@gmail.com>
2018-12-24 02:04:46 -0300

    dependency injection spring

---
+++ dependencyinjection/src/main/java/com/spring/di/dependencyinjection/clients/ClientForAnimal.java
@@ -0,0 +1,24 @@
+package com.spring.di.dependencyinjection.clients;
+
+import com.spring.di.dependencyinjection.entities.Animal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientForAnimal {
+
+    private Animal animal;
+
+    @Autowired
+    public ClientForAnimal(@Qualifier("Dog") Animal animal) {
+        this.animal = animal;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientForAnimal{" +
+                "animal=" + animal.makeNoise() +
+                '}';
+    }
+}
