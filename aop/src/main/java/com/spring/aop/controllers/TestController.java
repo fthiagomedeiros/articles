c61894544b98734b66666c801b9bdb34ce27d8e0
Francisco Thiago <francisco.medeiros@virtus.ufcg.edu.br>
2019-02-20 17:44:33 -0300

    Spring AOP Sample

---
+++ aop/src/main/java/com/spring/aop/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.spring.aop.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> testing() {
+        return new ResponseEntity<>("Testing", HttpStatus.OK);
+    }
+}
